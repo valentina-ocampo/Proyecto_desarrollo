d1f261900a12ae9e9fa8db11e27d1ef1a206a6cf
valentina-ocampo <valentinaocampo9491@gmail.com>
2021-03-31 00:01:47 -0500

    Se crea el template de la página de inicio.

--- aplicacion_blog/urls.py
+++ aplicacion_blog/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
-    path('blog/', include('apps.blog.urls')),
-    path('usuario/', include('apps.usuario.urls'))
+    # path('blog/', include('apps.blog.urls')),
+    # path('usuario/', include('apps.usuario.urls'))
 ]
