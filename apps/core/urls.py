bc9ea5ca55a517d79b1f5bbbe6cf87400c8c5b37
valentina-ocampo <valentinaocampo9491@gmail.com>
2021-04-04 22:53:17 -0500

    Se organizan las urls de Core.

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import HomePageView
 
+# Create your urls here.
+
+
+app_name = 'core'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
