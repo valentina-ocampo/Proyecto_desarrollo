637ce30c4dee39d545596e35a54f1c228157a417
valentina-ocampo <valentinaocampo9491@gmail.com>
2021-04-04 22:56:16 -0500

    Se crea la vista para Crear un usuario y la vista para mostrar sus datos.

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .forms import UserCreationFormWithOtherFields
+from django.views.generic import CreateView, TemplateView
+from django.contrib import messages
+from django.urls import reverse_lazy
+from .models import User
 
 # Create your views here.
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationFormWithOtherFields
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('core:home')
+
+    def get_success_url(self):
+        messages.success(self.request, 'Usuario creado satisfactoriamente.')
+        return super().get_success_url()
+
+class UserDetailView(TemplateView):
+    template_name = 'registration/user_detail.html'
