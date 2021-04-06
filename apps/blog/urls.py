328de523a2658acf1914e02c5ea185b3f0253f95
valentina-ocampo <valentinaocampo9491@gmail.com>
2021-04-06 00:19:31 -0500

    urls locales de blog.

---
+++ apps/blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import ListBlogsView, DetailBlogView, CreateBlogView, UpdateBlogView, DeleteBlogView
+
+# Create your urls here.
+
+
+app_name = 'blogs'
+
+urlpatterns = [
+    path('', ListBlogsView.as_view(), name='list_blogs'),
+    path('create/', CreateBlogView.as_view(), name='create_blog'),
+    path('<int:pk>/<slug:blog_slug>/', DetailBlogView.as_view(), name='read_blog'),
+    path('update/<int:pk>/<slug:blog_slug>/', UpdateBlogView.as_view(), name='update_blog'),
+    path('delete/<int:pk>/<slug:blog_slug>/', DeleteBlogView.as_view(), name='delete_blog'),
+]
