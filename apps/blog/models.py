82fa34f121a066eac877d28e6bfc829ae89da8fd
valentina-ocampo <valentinaocampo9491@gmail.com>
2021-04-06 00:19:13 -0500

    Modelos de blog.

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from apps.usuario.models import User
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    titulo = models.CharField(verbose_name="Título", max_length=200)
+    contenido = RichTextField(verbose_name="Contenido")
+    fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    fecha_actualizacion = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    reportada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Favoritos(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    pagina = models.ForeignKey(Blog, on_delete=models.CASCADE) 
