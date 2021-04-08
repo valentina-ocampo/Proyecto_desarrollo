ae389cc5aad65cd4b8f1b8aacf78e29f46043cf1
Carlos-ocampo <sandholcarl@hotmail.com>
2021-04-07 23:47:33 -0500

    Admin Eliminar Blogs

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListBlogsView, ListAllBlogsView, DetailBlogView, CreateBlogView, UpdateBlogView, DeleteBlogView
+from .views import ListBlogsView, ListAllBlogsView, DetailBlogView, CreateBlogView, UpdateBlogView, DeleteBlogView, reportBlog, reportListBlog, DeleteReportView
 
 # Create your urls here.
 
@@ -13,4 +13,7 @@ urlpatterns = [
     path('<int:pk>/<slug:blog_slug>/', DetailBlogView.as_view(), name='read_blog'),
     path('update/<int:pk>/<slug:blog_slug>/', UpdateBlogView.as_view(), name='update_blog'),
     path('delete/<int:pk>/<slug:blog_slug>/', DeleteBlogView.as_view(), name='delete_blog'),
+    path('delete/Report/<int:pk>/<slug:blog_slug>/', DeleteReportView.as_view(), name='delete_blog_reporter'),
+    path('report/<int:blogId>/', reportBlog , name='report_blog'),
+    path('reportList/', reportListBlog , name='report_list_blogs'),
 ]
